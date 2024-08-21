4025c9b8ae45f763d9597dfff0f7c07d71b54f10
Adedayo <adedayo.dc.sanni@gmail.com>
2024-08-20 21:25:49 -0300

    add teste com threads e correção makefile

--- philo/0_main.c
+++ philo/0_main.c
@@ -1,18 +1,37 @@
 #include "philo.h"
 
 
-int	main(int argc, char **argv)
+// int	main(int argc, char **argv)
+// {
+// 	if (argc < 5 || argc > 6)
+// 	{
+// 		printf("Wrong number of arguments\nThe program should be run as follows:\n./philo number_of_philosophers time_to_die time_to_eat time_to_sleep [number_of_times_each_philosopher_must_eat]\n");
+// 		return (1);
+// 	}
+// 	if (validate_args(argv) == 1)
+// 	{
+// 		printf("Invalid arguments!");
+// 		return (1);
+// 	}
+// 	//função que vai dar inicio a tudo
+// 	return(0);
+// }
+
+void* routine()
 {
-	if (argc < 5 || argc > 6)
-	{
-		printf("Wrong number of arguments\nThe program should be run as follows:\n./philo number_of_philosophers time_to_die time_to_eat time_to_sleep [number_of_times_each_philosopher_must_eat]\n");
-		return (1);
-	}
-	if (validate_args(argv) == 1)
-	{
-		printf("Invalid arguments!");
-		return (1);
-	}
-	//função que vai dar inicio a tudo
-	return(0);
+	printf("teste de thread\n");
+	sleep(3);
+	printf("final do teste de thread\n");
+	return (NULL);
 }
+
+int main(void)
+{
+	pthread_t t1, t2;
+	pthread_create(&t1, NULL, &routine, NULL);
+	pthread_create(&t2, NULL, &routine, NULL);
+	pthread_join(t1, NULL);
+	pthread_join(t2, NULL);
+	return 0;
+}
+//continuar playlist do codevault
