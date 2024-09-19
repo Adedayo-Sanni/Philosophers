62cc07a2e48b3eb6a7cf1832b53151ff764b1515
Adedayo <adedayo.dc.sanni@gmail.com>
2024-09-19 20:19:57 -0300

    add novas funções e arquivos

--- philo/2_init.c
+++ philo/2_init.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   2_init.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: asanni <asanni@student.42.fr>              +#+  +:+       +#+        */
+/*   By: adedayo <adedayo@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/14 17:48:32 by asanni            #+#    #+#             */
-/*   Updated: 2024/09/17 20:04:30 by asanni           ###   ########.fr       */
+/*   Updated: 2024/09/19 20:15:23 by adedayo          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,6 +30,27 @@ void	start_data(t_dinner *dinner, char **argv, int argc)
 		dinner->data->nb_meals_todo = ft_atol(argv[5]);
 }
 
-// void	start_philos(t_dinner *dinner)
-// {
-// }
+void	init_philos(t_dinner *dinner)
+{
+	int i;
+
+	i = 0;
+	while (i < dinner->data->nb_philos)
+	{
+		dinner->philos[i].philo_id = i + 1;
+		dinner->philos[i].left_fork = &dinner->forks[i];
+		dinner->philos[i].right_fork = &dinner->forks[(i + 1) % dinner->data->nb_philos];
+		dinner->philos[i].meals_had = 0;
+		dinner->philos[i].time_last_meal = 0;
+		dinner->philos[i].data = dinner->data;
+		i++;
+	}
+}
+
+void init_forks(t_dinner *dinner, int i)
+{
+    if (i >= dinner->data->nb_philos)
+        return;
+    pthread_mutex_init(&dinner->forks[i].mutex, NULL);
+    init_forks(dinner, i + 1);
+}
